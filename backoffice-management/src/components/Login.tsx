53682e34a8bc23e361cec3caa9825b80811f3661
Colin Hammarberg <colin.hammarberg@ecomero.com>
2024-12-05 13:46:02 +0100

    cors

--- backoffice-management/src/components/Login.tsx
+++ backoffice-management/src/components/Login.tsx
@@ -19,6 +19,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
         },
         body: JSON.stringify({
           email,
